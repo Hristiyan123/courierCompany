c7e77a15818a89431a24c52bb5134ef3fb6c537b
Hristiyan Velikov <135253187+Hristiyan123@users.noreply.github.com>
2024-03-19 23:08:07 +0200

    Update courier_dashboard.php
    Adding AJAX

--- dashboard/courier_dashboard.php
+++ dashboard/courier_dashboard.php
@@ -1,9 +1,6 @@
 <?php
-
 include_once 'C:/xampp/htdocs/EVROPUT-2000/includes/config.php';
-
 include_once 'C:/xampp/htdocs/EVROPUT-2000/includes/change_cars.php';
-
 include_once 'C:/xampp/htdocs/EVROPUT-2000/includes/delete_couriers_from_cars.php';
 ?>
 
@@ -13,35 +10,34 @@ include_once 'C:/xampp/htdocs/EVROPUT-2000/includes/delete_couriers_from_cars.ph
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Courier Dashboard</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="../ajax_communication/ajax_car_list.js"></script>
 </head>
 <body>
 
-<form action="" method="post">
-    <button type="submit" name="showCars">Show Cars</button>
+<form id="showCarsForm" method="post">
+    <button type="submit" name="showCars" id="showCars">Show Cars</button>
 </form>
 
-<?php
+<div id="carListContainer"></div>
 
-if (isset($_POST['showCars'])) {
-    echo '<form action="" method="post">
-            <button type="submit" name="changeCars">Change</button>
-          </form>';
+<form method="post">
+    <button type="submit" name="changeCars">Change</button>
+</form>
 
-    echo  '<form action="" method="post">
+<form action="" method="post">
             <button type="submit" name="removeFromCar">Remove courier From car</button>
-          </form>';
+</form>';
 
-    include_once 'C:/xampp/htdocs/EVROPUT-2000/includes/car_list.php';
-};
-
+<?php
 if (isset($_POST['changeCars'])) {
-    echo '<form class="searchform" action="" method="post">
-    <label for="changeCourier">insert courier id:</label>
-    <input id="changeCourier" type="text" name="CourierId" placeholder="Courier">
-    <P> to </P
-    <label for="changeCar">car id:</label>
-    <input id="changeCar" type="text" name="CarId" placeholder="Car">
-    <button type="submit" name="submitChanges">Submit</button>
+    echo '<form class="searchform" method="post">
+        <label for="changeCourier">Insert courier id:</label>
+        <input id="changeCourier" type="text" name="CourierId" placeholder="Courier">
+        <p>To</p>
+        <label for="changeCar">Car id:</label>
+        <input id="changeCar" type="text" name="CarId" placeholder="Car">
+        <button type="submit" name="submitChanges" id="submitChanges">Submit</button>
     </form>';
 };
 
@@ -52,10 +48,7 @@ if (isset($_POST['removeFromCar'])) {
     <button type="submit" name="submitRemove">Submit</button>
     </form>';
 };
-
-
 ?>
-
 
 </body>
 </html>
